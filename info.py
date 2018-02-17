fcc0b0678126efaea85d7ec3d9222c99e0498198
Evgeny Gladkih <eogladkih@hotmail.com>
2018-02-17 18:32:37 +0300

    Добавил telegram

--- info.py
+++ info.py
@@ -3,7 +3,7 @@ user_info={"first_name":"Evgeny","last_name":"Gladkih"}
 name=input("введите имя:")
 user_info["first_name"]=name
 lastname=input("введите фамилию:")
-user_info["last_name"]="lastname"
+user_info["last_name"]=lastname
 
 
 
